7042a72406243f66939bb68e16c222bf63190cd3
Swapnil <swapnilgajbhiye12@gmail.com>
2025-02-02 15:25:47 +0530

    initial

--- str.py
+++ str.py
@@ -254,5 +254,6 @@ def update_dashboard(n, selected_strike):
     return fig, alert_message, stock_data.to_dict('records'), option_chain_data.to_dict('records')
 
 # Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment variable
+    app.run(host="0.0.0.0", port=port)
